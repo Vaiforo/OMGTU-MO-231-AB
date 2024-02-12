c6f6dab5e68413aaa79b68806b408a940a61bd3a
Bobkov Andrej <abobkof@mail.ru>
2024-02-12 17:53:40 +0600

    push

--- Practice/1.py
+++ Practice/1.py
@@ -1,46 +1,52 @@
-grafs = [[15, 1, 2],
-         [14, 1, 5],
-         [23, 1, 4],
-         [19, 2, 3],
-         [16, 2, 4],
-         [15, 2, 5],
-         [14, 3, 5],
-         [26, 3, 6],
-         [25, 4, 5],
-         [23, 4, 7],
-         [20, 4, 8],
-         [24, 5, 6],
-         [27, 5, 8],
-         [18, 5, 9],
-         [14, 7, 8],
-         [18, 8, 9],
-         ]
+def print_all(tree, nach):
+    out = []
+    if nach in tree:
+        for person in tree[nach]:
+            out += [person]
+            out += print_all(tree, person)
+        return out
+    else:
+        return out
 
-grafs.sort(key=lambda x: x[0])
 
-trees, was_checked, out_grafs = {}, set(), []
+def prog(file_in):
+    last_nach = ""
+    tree = {}
+    persons = {}
+    ok = False
+    for line in file_in.readlines():
+        line = line.rstrip("\n")
+        if ok:
+            nach = line
+        if line == "END":
+            ok = True
+            continue
+        if last_nach == "":
+            last_nach = line[:4]
+        else:
+            person = line[:4]
+            if last_nach not in tree:
+                tree[last_nach] = [person]
+            else:
+                tree[last_nach] += [person]
+            last_nach = ""
+        persons[line[:4]] = "Unknown Name" \
+            if len(line.split()) == 1 else line[5:]
+    if nach not in persons:
+        for person in persons:
+            if nach == persons[person]:
+                nach = person
+    print(nach)
+    out = sorted(print_all(tree, nach))
+    print(persons)
+    print(tree)
+    print(out)
 
-for graf in grafs:
-    if graf[1] not in was_checked and graf[2] not in was_checked:
-        trees[graf[1]] = [graf[1], graf[2]]
-        trees[graf[2]] = trees[graf[1]]
-    elif graf[1] not in was_checked:
-        trees[graf[2]] += [graf[1]]
-        trees[graf[1]] = trees[graf[2]]
-    elif graf[2] not in was_checked:
-        trees[graf[1]] += [graf[2]]
-        trees[graf[2]] = trees[graf[1]]
-    if graf[1] not in was_checked or graf[2] not in was_checked:
-        out_grafs += [graf]
-        was_checked.add(graf[1])
-        was_checked.add(graf[2])
 
-for graf in grafs:
-    if graf[2] not in trees[graf[1]]:
-        new_tree = trees[graf[1]] + trees[graf[2]]
-        for key in trees:
-            if graf[1] in trees[key] or graf[2] in trees[key]:
-                trees[key] = new_tree
-        out_grafs += [graf]
-
-print(sum(list(map(lambda x: x[0], out_grafs))))
+for i in range(4, 5):
+    name = "0" + str(i) if len(str(i)) == 1 else str(i)
+    with open(f"Компания ХХХ\input_s1_{name}.txt", encoding="utf8") as file:
+        f_in = file
+        with open(f"Компания ХХХ\output_s1_{name}.txt", encoding="utf8") as file:
+            # input_nach = file.readline()
+            print(prog(f_in))
--- Practice/3.py
+++ Practice/3.py
@@ -1,36 +1,3 @@
-grafs = [[15, 1, 2],
-         [14, 1, 5],
-         [23, 1, 4],
-         [19, 2, 3],
-         [16, 2, 4],
-         [15, 2, 5],
-         [14, 3, 5],
-         [26, 3, 6],
-         [25, 4, 5],
-         [23, 4, 7],
-         [20, 4, 8],
-         [24, 5, 6],
-         [27, 5, 8],
-         [18, 5, 9],
-         [14, 7, 8],
-         [18, 8, 9],
-         ]
+a = ["013", "003", "001"]
 
-grafs.sort(key=lambda x: x[0])
-
-out_grafs, was_checked = [], set()
-was_checked.add(grafs[0][1])
-was_checked.add(grafs[0][2])
-out_grafs += [grafs[0]]
-
-maxe = max(grafs, key=lambda x: x[1:])
-while len(was_checked) != max(maxe[1:]):
-    for graf in grafs[1:]:
-        if (graf[1] not in was_checked and graf[2] in was_checked) or \
-                (graf[1] in was_checked and graf[2] not in was_checked):
-            was_checked.add(graf[1])
-            was_checked.add(graf[2])
-            out_grafs += [graf]
-            break
-
-print(sum(list(map(lambda x: x[0], out_grafs))))
+print(sorted(a))
