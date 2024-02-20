dd82a3114b21e4722ae84ca370b9c40a1327b4e5
Bobkov Andrej <abobkof@mail.ru>
2024-02-20 22:52:26 +0600

    push

--- Alg-and-prog/Audits/Menu.cs
+++ Alg-and-prog/Audits/Menu.cs
@@ -1,17 +1,14 @@
 public class Menu {
-    public void skip() {
+    public void Skip() {
         Console.Write("Нажмите enter для продолжения...");
         Console.ReadLine();
     }
 
-    public bool checkEmptyCreatedBd(List<Auditorium> audits, bool checkEmpty) {
+    public bool CheckEmptyCreatedBd(List<Auditorium> audits, bool checkEmpty) {
         try {
-            if (!checkEmpty && audits.Count == 0) {
-                return true;
-            }
+            if (!checkEmpty && audits.Count == 0) return true;
             if (audits.Count == 0) {
-                Console.WriteLine("База данных пуста.");
-                Console.WriteLine("Сначала добавьте аудитории!");
+                Console.WriteLine("База данных пуста.\nСначала добавьте аудитории!");
                 return false;
             } return true;
         } catch {
@@ -30,18 +27,18 @@ public class Menu {
 
         Console.WriteLine("--Добавление новой аудитории--");
 
-        Console.Write("Введите номер аудитории (этаж+номер): ");
-        string err = newAudit.setNumber(Console.ReadLine());
-        while (err != "") {
-            Console.Write($"{err}\n\nВведите номер аудитории (этаж+номер): ");
-            err = newAudit.setNumber(Console.ReadLine());
+        while (true) {
+            Console.Write("Введите номер аудитории (этаж+номер): ");
+            var err = newAudit.setNumber(Console.ReadLine());
+            if (err != "") Console.WriteLine($"{err}\n");
+            else break;
         }
 
-        Console.Write("Введите кол-во посадочных мест: ");
-        err = newAudit.setSitCount(Console.ReadLine());
-        while (err != "") {
-            Console.Write($"{err}\n\nВведите кол-во посадочных мест: ");
-            err = newAudit.setSitCount(Console.ReadLine());
+        while (true) {
+            Console.Write("Введите кол-во посадочных мест: ");
+            var err = newAudit.setSitCount(Console.ReadLine());
+            if (err != "") Console.WriteLine($"{err}\n");
+            else break;
         }
 
         Console.WriteLine("Имеется ли проектор:");
@@ -69,13 +66,13 @@ public class Menu {
         Console.WriteLine("--Изменение данных об аудитории--");
 
         Console.Write("Введите номер аудитории: ");
-        string number = Console.ReadLine();
-        bool isNum = int.TryParse(number, out int num);
-        while (isNum) {
-            Console.WriteLine("Неверный ввод!");
+        int num;
+        while (true) {
             Console.Write("Введите номер аудитории: ");
-            number = Console.ReadLine();
-            isNum = int.TryParse(number, out num);
+            string number = Console.ReadLine();
+            bool isNum = int.TryParse(number, out num);
+            if (isNum) break;
+            else Console.WriteLine("Неверный ввод!");
         }
 
 
@@ -91,14 +88,12 @@ public class Menu {
             down = Console.CursorTop;
             if (Choose(ref top, ref y, ref down) == 1) return;
 
-            Console.Write("Введите номер аудитории: ");
-            number = Console.ReadLine();
-            isNum = int.TryParse(number, out num);
-            while (isNum) {
-                Console.WriteLine("Неверный ввод!");
+            while (true) {
                 Console.Write("Введите номер аудитории: ");
-                number = Console.ReadLine();
-                isNum = int.TryParse(number, out num);
+                string number = Console.ReadLine();
+                bool isNum = int.TryParse(number, out num);
+                if (isNum) break;
+                else Console.WriteLine("Неверный ввод!");
             }
 
             findedAudit = auditoriums.Find(elem => elem.getNumber() == num);
@@ -106,11 +101,11 @@ public class Menu {
 
         int index = auditoriums.IndexOf(findedAudit);
 
-        Console.Write($"Введите новое кол-во посадочных мест - текущее <{findedAudit.getSitCount()}>: ");
-        var err = auditoriums[index].setSitCount(Console.ReadLine());
-        while (err != "") {
-            Console.Write($"{err}\n\nВведите новое кол-во посадочных мест - текущее <{findedAudit.getSitCount()}>: ");
-            err = auditoriums[index].setSitCount(Console.ReadLine());
+        while (true) {
+            Console.Write($"Введите новое кол-во посадочных мест - текущее <{findedAudit.getSitCount()}>: ");
+            var err = auditoriums[index].setSitCount(Console.ReadLine());
+            if (err != "") Console.WriteLine($"{err}\n");
+            else break;
         }
 
         Console.WriteLine($"Имеется ли проектор - текущее <{(findedAudit.getProecktor() ? "да" : "нет")}>:");
@@ -133,14 +128,13 @@ public class Menu {
     public void printCountSitAudits(List<Auditorium> auditoriums) {
         Console.WriteLine("--Поиск по кол-ву посадочных мест--");
 
-        Console.Write("Введите кол-во посадочных мест: ");
-        string sitCount = Console.ReadLine();
-        bool isNum = int.TryParse(sitCount, out int num);
-        while (isNum) {
-            Console.WriteLine("Неверный ввод!");
-            Console.Write("Введите кол-во посадочных мест: ");
-            sitCount = Console.ReadLine();
-            isNum = int.TryParse(sitCount, out num);
+        int num;
+        while (true) {
+            Console.Write("Введите номер аудитории: ");
+            string number = Console.ReadLine();
+            bool isNum = int.TryParse(number, out num);
+            if (isNum) break;
+            else Console.WriteLine("Неверный ввод!");
         }
 
         bool haveAudits = false;
@@ -175,21 +169,22 @@ public class Menu {
     public void printCountSitPcAudits(List<Auditorium> auditoriums) {
         Console.WriteLine("--Поиск по кол-ву компьютеров--");
 
-        Console.Write("Введите кол-во компьютеров: ");
-        string sitCount = Console.ReadLine();
-        bool isNum = int.TryParse(sitCount, out int num);
-        while (isNum) {
-            Console.WriteLine("Неверный ввод!");
+        int num;
+        while (true) {
             Console.Write("Введите кол-во компьютеров: ");
-            sitCount = Console.ReadLine();
-            isNum = int.TryParse(sitCount, out num);
+            string number = Console.ReadLine();
+            bool isNum = int.TryParse(number, out num);
+            if (isNum) break;
+            else Console.WriteLine("Неверный ввод!");
         }
 
+        Console.WriteLine(num);
         bool haveAudits = false;
         Console.WriteLine("\nПодходящие аудитории:");
         foreach (Auditorium auditorium in auditoriums) {
             if (auditorium.getSitCount() >= num && auditorium.getHavePc()) {
                 haveAudits = true;
+                Console.WriteLine(auditorium.getSitCount());
                 Console.WriteLine(auditorium.getNumber());
             }
         }
@@ -201,20 +196,19 @@ public class Menu {
     public void printStageAudits(List<Auditorium> auditoriums) {
         Console.WriteLine("--Поиск по этажу--");
         
-        Console.Write("Введите этаж: ");
-        string stage = Console.ReadLine();
-        bool isNum = int.TryParse(stage, out int num);
-        while (isNum) {
-            Console.WriteLine("Неверный ввод!");
+        int stage;
+        while (true) {
             Console.Write("Введите этаж: ");
-            stage = Console.ReadLine();
-            isNum = int.TryParse(stage, out num);
+            string number = Console.ReadLine();
+            bool isNum = int.TryParse(number, out stage);
+            if (isNum) break;
+            else Console.WriteLine("Неверный ввод!");
         }
 
         bool haveAudits = false;
         Console.WriteLine("\nПодходящие аудитории:");
         foreach (Auditorium auditorium in auditoriums) {
-            if (int.Parse(auditorium.getNumber().ToString()[0] + "") == num) {
+            if (int.Parse(auditorium.getNumber().ToString()[0] + "") == stage) {
                 haveAudits = true;
                 Console.WriteLine(auditorium.getNumber());
             }
--- Alg-and-prog/Audits/Program.cs
+++ Alg-and-prog/Audits/Program.cs
@@ -49,56 +49,56 @@ public class Audits {
                 case 1:
                     auditoriums = menu.CreateBD();
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 2:
-                    if (menu.checkEmptyCreatedBd(auditoriums, false)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, false)) {
                         menu.CreateNewAudit(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 3:
-                    if (menu.checkEmptyCreatedBd(auditoriums, false)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, false)) {
                         menu.EditAuditByNumber(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 4:
-                    if (menu.checkEmptyCreatedBd(auditoriums, true)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, true)) {
                         menu.printCountSitAudits(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 5:
-                    if (menu.checkEmptyCreatedBd(auditoriums, true)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, true)) {
                         menu.printHaveProecktorAudits(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 6:
-                    if (menu.checkEmptyCreatedBd(auditoriums, true)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, true)) {
                         menu.printCountSitPcAudits(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 7:
-                    if (menu.checkEmptyCreatedBd(auditoriums, true)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, true)) {
                         menu.printStageAudits(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break;
                 case 8:
-                    if (menu.checkEmptyCreatedBd(auditoriums, true)) {
+                    if (menu.CheckEmptyCreatedBd(auditoriums, true)) {
                         menu.printAllAudits(auditoriums);
                     }
                     nowScreen = Screen.Menu;
-                    menu.skip();
+                    menu.Skip();
                     break; 
                 case 9:
                     Environment.Exit(0);
