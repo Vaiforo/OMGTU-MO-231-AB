a48c41350ac9b328c896ee19a222e9045e7b6fa7
Bobkov Andrej <abobkof@mail.ru>
2024-03-21 15:41:44 +0600

    hashset_done

--- Alg-and-prog/HashSet/Program.cs
+++ Alg-and-prog/HashSet/Program.cs
@@ -1,28 +1,54 @@
 ﻿class HashSetTester {
     static void Main() {
-        HashSet<int> hashSet1 = new(){1, 2, 3, 4, 5, 7, 9};  // Всегда больше всего элементов
-        HashSet<int> hashSet2 = new(){1, 2, 3, 8, 10};
-        HashSet<int> hashSet3 = new(){4, 5, 6, 7};  // Всегда меньше всего элементов
+        HashSet<int> hashSet1 = new(){1, 2, 3, 4, 7, 8, 10};  // Всегда больше всего элементов
+        HashSet<int> hashSet2 = new(){1, 3, 4, 6, 7, 9};
+        HashSet<int> hashSet3 = new(){1, 3, 4, 5, 6};  // Всегда меньше всего элементов
 
-        HashSet<int> intersection = new();
-        foreach (int item in hashSet1) {
-            if (hashSet2.Contains(item) && hashSet3.Contains(item)) intersection.Add(item);
-        }
+        DisplaySet(hashSet1, "Hashset 1:", false);
+        DisplaySet(hashSet2, "Hashset 2:", false);
+        DisplaySet(hashSet3, "Hashset 3:", true);
 
-        var intersection_auto = hashSet1.IntersectWith(hashSet2).IntersectWith(hashSet3);
+        // Пересечение
 
-        DisplaySet(intersection, intersection_auto, "Пересечение:");
+        HashSet<int> intersection = new(hashSet1);
+        intersection.IntersectWith(hashSet2);
+        intersection.IntersectWith(hashSet3);
+
+        DisplaySet(intersection, "Пересечение:", true);
+
+        // Объединение
+
+        HashSet<int> union = new(hashSet1);
+        union.UnionWith(hashSet2);
+        union.UnionWith(hashSet3);
+
+        DisplaySet(union, "Объединение:", true);
+
+        // Дополнение
+
+        HashSet<int> addition1 = new(union);
+        bool inHashset1(int i) => hashSet1.Contains(i);
+        addition1.RemoveWhere(inHashset1);
+
+        DisplaySet(addition1, "Дополнение к 1:", true);
+
+        HashSet<int> addition2 = new(union);
+        bool inHashset2(int i) => hashSet2.Contains(i);
+        addition2.RemoveWhere(inHashset2);
+
+        DisplaySet(addition2, "Дополнение к 2:", true);
+
+        HashSet<int> addition3 = new(union);
+        bool inHashset3(int i) => hashSet3.Contains(i);
+        addition3.RemoveWhere(inHashset3);
+
+        DisplaySet(addition3, "Дополнение к 3:", true);
     }
 
-    static void DisplaySet(HashSet<int> collection1, IEnumerable<int> collection2, string message) {
+    static void DisplaySet(HashSet<int> collection, string message, bool n) {
         Console.WriteLine(message);
-        Console.WriteLine("Вручную:");
-        foreach (int item in collection1) {
+        foreach (int item in collection) {
             Console.Write($"{item} ");
-        }
-        Console.WriteLine("\nМетодом:");
-        foreach (int item in collection2) {
-            Console.Write($"{item} ");
-        }
+        } Console.WriteLine(n ? "\n" : "");
     }
 }
