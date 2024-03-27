94f99f8a02c3cc4905ac43190762d2e1e00907f1
Bobkov Andrej <abobkof@mail.ru>
2024-03-27 10:03:03 +0600

    push

--- Practice/5.py
+++ Practice/5.py
@@ -1,17 +1,55 @@
-from math import inf
+def prog(input_data):
+    pos1 = int(input_data[0]) + 1
+    magic_words = []
+    for j in range(1, pos1):
+        magic_words.append(input_data[j])
+    f_crit = []
+    pos2 = pos1 + int(input_data[pos1]) + 1
+    for j in range(pos1 + 1, pos2):
+        f_crit.append(input_data[j])
+    pos1 = pos2
+    pos2 = pos2 + int(input_data[pos1])
+    l_crit = []
+    for j in range(pos1 + 1, pos2 + 1):
+        l_crit.append(input_data[j])
 
-grafs = [
-    [inf, 1, inf, inf, 3],
-    [inf, inf, 8, 7, 1],
-    [inf, inf, inf, 1, -5],
-    [inf, inf, 2, inf, inf],
-    [inf, inf, inf, 4, inf],
-]
+    n_out = 0
+    start_crit = {}
+    end_crit = {}
 
-lambdas = [inf if i > 0 else 0 for i in range(len(grafs))]
+    for elem in f_crit:
+        elem = elem.split()
+        start_crit[elem[0]] = int(elem[1])
+    for elem in l_crit:
+        elem = elem.split()
+        end_crit[elem[0]] = int(elem[1])
 
-for i in range(1, len(grafs)):
-    for j in range(1, len(grafs)):
-        lambdas[j] = min(list(map(lambda x: lambdas[x] + grafs[x][j], range(len(grafs)))))
+    for elem in magic_words:
+        try:
+            start_crit[elem[0]] -= 1
+            if start_crit[elem[0]] < 0:
+                raise KeyError
+            try:
+                end_crit[elem[-1]] -= 1
+                if end_crit[elem[-1]] < 0:
+                    raise KeyError
+                n_out += 1
+            except KeyError:
+                start_crit[elem[0]] += 1
+        except KeyError:
+            pass
+    return n_out
 
-print(*lambdas)
+
+for i in range(1, 14):
+    strok: str = f"0{i}" if i < 10 else i
+    with open(f"Золотая рыбка\input_s1_{strok}.txt", encoding="utf8") as file:
+        data = file.readlines()
+        data = list(map(lambda x: x.rstrip("\n"), data))
+        answer: int = prog(data)
+    with open(f"Золотая рыбка\output_s1_{strok}.txt", encoding="utf8") as file:
+        data: int = int(file.readline())
+        print(i)
+        print(f"right: {data} my: {answer}")
+        print(data == answer)
+        print()
